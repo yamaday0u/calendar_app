822b13a4a8bdd3aae64130be714af8708e6828ac
yamaday0u <35hallelujah00@gmail.com>
2020-10-28 22:52:47 +0900

    2nd commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   get "/" => "home#top"
 
-  get "blogs/index"
 
-  get "users/new" => "users#new"
-  get "users/create" => "users#create"
+  get "signup" => "users#new"
+  post "users/create" => "users#create"
   get "login" => "users#login_form"
   post "login" => "users#login"
   post "logout" => "users#logout"
+
+  get "blogs/index/:id" => "blogs#index"
+  resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
